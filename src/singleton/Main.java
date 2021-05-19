b40b18198776209479ad20d467fba20c4aece445
SuperDaHei <guix1996@163.com>
2021-05-19 21:57:27 +0800

    Singleton(单例)模式

---
+++ src/singleton/Main.java
@@ -0,0 +1,12 @@
+package singleton;
+
+/**
+ * @Description TODO
+ * @Author SuperDaHei
+ * @Date 2021-05-19 21:54
+ */
+public class Main {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.createSingleton();
+    }
+}
---
+++ src/singleton/Singleton.java
@@ -0,0 +1,21 @@
+package singleton;
+
+/**
+ * @Description TODO
+ * @Author SuperDaHei
+ * @Date 2021-05-19 21:51
+ */
+public class Singleton {
+
+    private static Singleton singleton;
+
+    private Singleton() {
+    }
+
+    public static Singleton createSingleton() {
+        if (singleton == null) {
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+}
